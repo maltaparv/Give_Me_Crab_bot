772fadc959dc19dfe2f8c6b41014b2b341fc887b
Gennady Guniashev <68774508+Genateep@users.noreply.github.com>
2021-04-15 09:01:04 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-BOT_TOKEN = '1778430186:AAF1pn8BVUtRnn6mBt7d-UitWNBYHadShws'  # токен бота в Телеграм
-admin_id = '393391732'  # идентификатор владельца бота в Телеграм
+BOT_TOKEN = ''  # токен бота в Телеграм
+admin_id = ''  # идентификатор владельца бота в Телеграм
 
 # сервисный ключ доступа для vk_api
-service_key = '86b6e1a486b6e1a486b6e1a42686c00d9d886b686b6e1a4e6e7316e6c0f87581a418da6'
+service_key = ''
